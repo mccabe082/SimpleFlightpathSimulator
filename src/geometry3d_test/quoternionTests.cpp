135e242190b382981da01db78a91fbfdccfbe0cf
david.mccabe <david.mccabe082@gmail.com>
2020-04-20 23:00:28 +0100

    added additional tests which are probably unnessesary

--- src/geometry3d_test/quoternionTests.cpp
+++ src/geometry3d_test/quoternionTests.cpp
@@ -55,40 +55,6 @@ TEST_CASE("Quoternion arithmetic is correct", "[quoternion]") {
 		REQUIRE(q.yaw() == Approx(yaw));
 	}
 
-	SECTION("equivalence check is correct") {
-		Quoternion q1(1., 2., 3., 4.);
-		Quoternion q2(4., 3., 2., 1.);
-
-		REQUIRE(q1 == q1);
-		REQUIRE(!(q1 == q2));
-	}
-
-	SECTION("quoternian addition is correct") {
-		Quoternion q1(1., 2., 3., 4.);
-		Quoternion q2(4., 3., 2., 1.);
-		Quoternion q3(5., 5., 5., 5.);
-
-		REQUIRE(q1 + q2 == q3);
-	}
-
-	SECTION("quoternian multiplication is correct") {
-		// https://uk.mathworks.com/help/aerotbx/ug/quatmultiply.html
-		Quoternion q1(1., 0., 1., 0.);
-		Quoternion q2(1., 0.5, 0.5, 0.75);
-		Quoternion q3(0.5, 1.25, 1.5, 0.25);
-
-		REQUIRE(q1 * q2 == q3);
-	}
-
-	SECTION("quoternian scalar multiplication is correct") {
-		// https://uk.mathworks.com/help/aerotbx/ug/quatmultiply.html
-		Quoternion q1(1., 2., 3., 4.);
-		Quoternion q2(10., 20., 30., 40.);
-
-		REQUIRE(q1 * 10. == q2);
-		REQUIRE(q2 == q1 * 10.);
-	}
-
 	SECTION("conjugate calculation is correct") {
 		Quoternion q(1., 2., 3., 4.);
 		Quoternion qExpectedConj(1., -2., -3., -4.);
@@ -107,7 +73,7 @@ TEST_CASE("Quoternion arithmetic is correct", "[quoternion]") {
 		REQUIRE(q.inverse() == qExpectedInv);
 	}
 
-	SECTION("has Norm") {
+	SECTION("has unity norm check") {
 		double c = std::sqrt(0.25);
 		Quoternion qNormEq1(c, c, c, c);
 		REQUIRE(qNormEq1.hasUnityNorm());
@@ -124,5 +90,46 @@ TEST_CASE("Quoternion arithmetic is correct", "[quoternion]") {
 		REQUIRE(!qNonPure.isPure());
 	}
 
+	SECTION("equivalence check is correct") {
+		Quoternion q1(1., 2., 3., 4.);
+		Quoternion q2(4., 3., 2., 1.);
+
+		REQUIRE(q1 == q1);
+		REQUIRE(!(q1 == q2));
+	}
+
+	SECTION("quoternian addition is correct") {
+		Quoternion q1(1., 2., 3., 4.);
+		Quoternion q2(4., 3., 2., 1.);
+		Quoternion q3(5., 5., 5., 5.);
+
+		REQUIRE(q1 + q2 == q3);
+	}
+
+	SECTION("quoternian subtration is correct") {
+		Quoternion q1(1., 2., 3., 4.);
+		Quoternion q2(4., 3., 2., 1.);
+		Quoternion q3(-3., -1., 1., 3.);
+
+		REQUIRE(q1 - q2 == q3);
+	}
+
+	SECTION("quoternian multiplication is correct") {
+		// https://uk.mathworks.com/help/aerotbx/ug/quatmultiply.html
+		Quoternion q1(1., 0., 1., 0.);
+		Quoternion q2(1., 0.5, 0.5, 0.75);
+		Quoternion q3(0.5, 1.25, 1.5, 0.25);
+
+		REQUIRE(q1 * q2 == q3);
+	}
+
+	SECTION("quoternian scalar multiplication is correct") {
+		// https://uk.mathworks.com/help/aerotbx/ug/quatmultiply.html
+		Quoternion q1(1., 2., 3., 4.);
+		Quoternion q2(10., 20., 30., 40.);
+
+		REQUIRE(q1 * 10. == q2);
+		REQUIRE(10. * q1 == q2);
+	}
 
 }
