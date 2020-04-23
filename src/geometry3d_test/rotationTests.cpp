c6cbc38bc8e77a7c4b327031fc6f55ae85c11dec
david.mccabe <david.mccabe082@gmail.com>
2020-04-23 03:52:54 +0100

    added rotation unit tests

--- src/geometry3d_test/rotationTests.cpp
+++ src/geometry3d_test/rotationTests.cpp
@@ -1,5 +1,6 @@
 #include "catch.hpp"
 #include <geometry3d/rotation.h>
+#include <geometry3d/orientation.h>
 #include <geometry3d/quaternion.h>
 #include <cmath>
 
@@ -13,21 +14,45 @@ namespace
 TEST_CASE("Rotation arithmetic is correct", "[rotation]") {
 
 	SECTION("interpolation works correctly") {
-		
-        const double degPerSec4pt5 = pi / 40.;
+
+		const double degPerSec4pt5 = pi / 40.;
 
 		Rotation r1(0., 0., 0.);
 		Rotation r2(0., degPerSec4pt5, 0.);
-		Rotation rExpected(0., degPerSec4pt5/2., 0.);
-		Rotation rActual = Rotation::interpolate(r1,r2,0.5);
+		Rotation rExpected(0., degPerSec4pt5 / 2., 0.);
+		Rotation rActual = Rotation::interpolate(r1, r2, 0.5);
 
 		REQUIRE(rExpected.pitchRate() == Approx(rActual.pitchRate()));
 		REQUIRE(rExpected.rollRate() == Approx(rActual.rollRate()));
 		REQUIRE(rExpected.yawRate() == Approx(rActual.yawRate()));
 	}
 
+	SECTION("calculation from change in orientation works correctly") {
 
-		// static Rotation from(const Orientation& oStart, const Orientation& oFinal, double time);
-		// static Rotation average(const Rotation& start, const Rotation & final
+		const double degPerSec4pt5 = pi / 40.;
+
+		Orientation theta0(0., 0., 0.);
+		Orientation theta1(0., degPerSec4pt5, 0.);
+		Rotation thetaDotExpected(0., degPerSec4pt5, 0.);
+		Rotation thetaDotActual = Rotation::from(theta0, theta1, 1.);
+
+		REQUIRE(thetaDotExpected.pitchRate() == Approx(thetaDotActual.pitchRate()));
+		REQUIRE(thetaDotExpected.rollRate() == Approx(thetaDotActual.rollRate()));
+		REQUIRE(thetaDotExpected.yawRate() == Approx(thetaDotActual.yawRate()));
+	}
+
+	SECTION("averaging works correctly") {
+
+		const double degPerSec4pt5 = pi / 40.;
+
+		Rotation r1(0., 0., 0.);
+		Rotation r2(0., degPerSec4pt5, 0.);
+		Rotation rExpected(0., degPerSec4pt5 / 2., 0.);
+		Rotation rActual = Rotation::average(r1, r2);
+
+		REQUIRE(rExpected.pitchRate() == Approx(rActual.pitchRate()));
+		REQUIRE(rExpected.rollRate() == Approx(rActual.rollRate()));
+		REQUIRE(rExpected.yawRate() == Approx(rActual.yawRate()));
+	}
 
 }
