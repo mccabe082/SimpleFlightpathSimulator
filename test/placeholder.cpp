25f02836edca47f6bb20be7669fd0ad77b7fd22c
david.mccabe <david.mccabe082@gmail.com>
2020-04-28 13:13:56 +0100

    finally got the config script working with correct install directory

--- test/placeholder.cpp
+++ test/placeholder.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include <simple_flightpath_sim/flightpath.h>
+#include <flightpath.h>
 
 TEST_CASE("Provisional end-to-end tests", "[placeholder]" ) {
 
