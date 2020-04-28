1b30fed72468967b0d40974c1ac3fea0cea110ad
david.mccabe <david.mccabe082@gmail.com>
2020-04-28 22:50:09 +0100

    sweepup prior to fork

--- src/simple_waypoint_sim/simple_waypoint_sim.cpp
+++ src/simple_waypoint_sim/simple_waypoint_sim.cpp
@@ -1,23 +1,15 @@
 #include "simple_waypoint_sim/simple_waypoint_sim.h"
 #include <string>
 #include "waypoint_queue.h"
+#include "follow_waypoints.h"
 
 namespace SimpleWaypointSim
 {
     void* loadWaypointSim(const char* waypointQueueFile)
     {
-	try
-	{
-		std::string fStr = waypointQueueFile;
-		SimpleWaypointSim::WaypointQueue waypoints;
-		bool ok = SimpleWaypointSim::WaypointQueue::readFromXML(waypoints, fStr);
-		static int test = 12345;
-		return &test;
-	}
-	catch (...)
-	{
-
-	}
+		WaypointQueue waypoints;
+		SimpleWaypointSim::WaypointQueue::readFromXML(waypoints, std::string(waypointQueueFile));
+		return new FollowWaypoints(waypoints);
     }
     
     bool loadFlightpath(void* handle)
