04e00b828b3cf72445a71ea20eb732a0a878cee2
david.mccabe <david.mccabe082@gmail.com>
2020-04-29 12:34:08 +0100

    basic end to end test seems to work

--- test/flightpath.test.cpp
+++ test/flightpath.test.cpp
@@ -2,13 +2,46 @@
 #include "catch.hpp"
 #include <flightpath.h>
 
-TEST_CASE("Provisional end-to-end tests", "[placeholder]" ) {
+TEST_CASE("Provisional end-to-end tests", "[end-to-end]" ) {
 
-	void* handle = loadFlightpathSim("sillyTest.xml");
+	void* iSim = loadFlightpathSim("D:/simple-flightpath-simulator/test/example_waypoint_queue.xml");
 
-    SECTION( "report project existance" ) {
-        bool projectExists = true;
-        REQUIRE(projectExists);
+    SECTION( "basic check to ensure library runs end-to-end" ) {
+
+        REQUIRE(iSim);
+
+    	double x, y, z;
+	    double pitch, roll, yaw;
+
+		bool status_ok = true;
+		
+		status_ok = advanceWaypointSim(iSim, 0.5, x, y, z, pitch, roll, yaw);
+
+		REQUIRE(x == Approx(1.));
+		REQUIRE(y == Approx(1.5));
+		REQUIRE(z == Approx(.0));
+		REQUIRE(roll == Approx(.15).epsilon(.001));
+		REQUIRE(pitch == Approx(.0).epsilon(.001));
+		REQUIRE(yaw == Approx(.0).epsilon(.001));
+
+		status_ok = advanceWaypointSim(iSim, 0.5, x, y, z, pitch, roll, yaw);
+
+		REQUIRE(x == Approx(1.5));
+		REQUIRE(y == Approx(1.5));
+		REQUIRE(z == Approx(.0));
+		REQUIRE(roll == Approx(.2).epsilon(.001));
+		REQUIRE(pitch == Approx(.0).epsilon(.001));
+		REQUIRE(yaw == Approx(.0).epsilon(.001));
+
+		status_ok = advanceWaypointSim(iSim, 0.5, x, y, z, pitch, roll, yaw);
+
+		REQUIRE(x == Approx(2.));
+		REQUIRE(y == Approx(1.5));
+		REQUIRE(z == Approx(.0));
+		REQUIRE(roll == Approx(.25).epsilon(.001));
+		REQUIRE(pitch == Approx(.0).epsilon(.001));
+		REQUIRE(yaw == Approx(.0).epsilon(.001));
+
     }
 
 }
