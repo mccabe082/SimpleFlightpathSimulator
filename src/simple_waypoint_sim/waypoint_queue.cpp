fa5ce2e73f0efb74041e5d02e089ae5e26c4939a
david.mccabe <david.mccabe082@gmail.com>
2020-04-30 16:42:44 +0100

    added some provision for unity position and orientation inversion

--- src/simple_waypoint_sim/waypoint_queue.cpp
+++ src/simple_waypoint_sim/waypoint_queue.cpp
@@ -8,11 +8,37 @@
 
 namespace
 {
+	const double pi = std::acos(-1.);
+
+	double deg2red(double alpha)
+	{
+		return pi * alpha / 180.;
+	}
+
+	bool hasAttribute(rapidxml::xml_node<>* node, const char* attr)
+	{
+		return node->first_attribute(attr);
+	}
+
+	const char* getAttribute(rapidxml::xml_node<>* node, const char* attr)
+	{
+		return hasAttribute(node, attr) ? node->first_attribute(attr)->value() : nullptr;
+	}
 
 	void dirtyWaypointQueueReading(SimpleWaypointSim::WaypointQueue& waypoints, const std::string& fileName)
 	{
 		using namespace SimpleWaypointSim;
 		using namespace rapidxml;
+
+		bool pitchInverted = false;
+		bool rollInverted = false;
+		bool yawInverted = false;
+		bool pitchDegrees = false;
+		bool rollDegrees = false;
+		bool yawDegrees = false;
+		bool xInverted = false;
+		bool yInverted = false;
+		bool zInverted = false;
 
 		WaypointQueue wpBuffer;
 		try
@@ -24,6 +50,18 @@ namespace
 			xml_node<>* waypointQueueNode = doc.first_node("WaypointQueue");
 			if (waypointQueueNode)
 			{
+				{
+					pitchInverted = getAttribute(waypointQueueNode, "");
+					rollInverted = getAttribute(waypointQueueNode, "");
+					yawInverted = getAttribute(waypointQueueNode, "");
+					pitchDegrees = getAttribute(waypointQueueNode, "");
+					rollDegrees = getAttribute(waypointQueueNode, "");
+					yawDegrees = getAttribute(waypointQueueNode, "");
+					xInverted = getAttribute(waypointQueueNode, "");
+					yInverted = getAttribute(waypointQueueNode, "");
+					zInverted = getAttribute(waypointQueueNode, "");
+				}
+
 				xml_node<>* waypointNode = waypointQueueNode->first_node("Waypoint");
 				while (waypointNode)
 				{
@@ -34,12 +72,24 @@ namespace
 					double y = atof(positionNode->first_attribute("y")->value());
 					double z = atof(positionNode->first_attribute("z")->value());
 
+					if (xInverted) x *= -1.;
+					if (yInverted) y *= -1.;
+					if (zInverted) z *= -1.;
+
 					xml_node<>* orientationNode = waypointNode->first_node("Orientation");
 					double pitch = atof(orientationNode->first_attribute("pitch")->value());
 					double roll = atof(orientationNode->first_attribute("roll")->value());
 					double yaw = atof(orientationNode->first_attribute("yaw")->value());
-					
-					wpBuffer.addWaypoint(Waypoint(Position(x,y,z), Orientation(pitch,roll,yaw), flyThroughTime));
+
+					if (pitchDegrees) x = deg2red(pitch);
+					if (rollDegrees) x = deg2red(roll);
+					if (yawDegrees) x = deg2red(yaw);
+
+					if (pitchInverted) pitch *= -1.;
+					if (rollInverted)  roll *= -1.;
+					if (yawInverted)   yaw *= -1.;
+
+					wpBuffer.addWaypoint(Waypoint(Position(x, y, z), Orientation(pitch, roll, yaw), flyThroughTime));
 
 					waypointNode = waypointNode->next_sibling("Waypoint");
 				}
@@ -90,7 +140,7 @@ namespace SimpleWaypointSim
 
 	std::optional<Waypoint> WaypointQueue::operator[](unsigned int iWaypoint) const
 	{
-		return _data.size()>=iWaypoint ? std::optional<Waypoint>(_data[iWaypoint]) : std::nullopt;
+		return _data.size() >= iWaypoint ? std::optional<Waypoint>(_data[iWaypoint]) : std::nullopt;
 	}
 
 	unsigned WaypointQueue::waypointsRemaining(double simTime) const
